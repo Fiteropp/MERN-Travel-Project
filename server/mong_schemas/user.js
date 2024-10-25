4fb00dba2739fc03d42fdb977da30c86764bc183
Fiteropp <140279026+Fiteropp@users.noreply.github.com>
2024-10-25 11:00:59 +0300

    MongoDB and Mongoose Setup + File Hierarchy
    Created file hierarchy, and created 1 mongoose schema.

---
+++ server/mong_schemas/user.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+const {Schema, model} = mongoose;
+
+const userSchema = new Schema ({
+    username: String,
+    email: String,
+    password: String,
+    role: Role
+});
+
+const User =model('User', userSchema);
+export default User;
