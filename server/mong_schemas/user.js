bed4067f7ae22939fcc3e1e9b457a7e3c932124f
Fiteropp <140279026+Fiteropp@users.noreply.github.com>
2024-10-25 13:38:48 +0300

    Changed User Schema

--- server/mong_schemas/user.js
+++ server/mong_schemas/user.js
@@ -2,10 +2,19 @@ import mongoose from "mongoose";
 const {Schema, model} = mongoose;
 
 const userSchema = new Schema ({
+    
     username: String,
     email: String,
     password: String,
-    role: Role
+    
+    roleAndRights:{
+        role: String,
+        create: Boolean,
+        read: Boolean,
+        update: Boolean,
+        delete: Boolean,
+    } 
+    
 });
 
 const User =model('User', userSchema);
