caeff7422643cf77930330da7d8a37d664b20bd3
Fiteropp <140279026+Fiteropp@users.noreply.github.com>
2024-11-14 14:22:59 +0200

    Mod page | React

---
+++ client/react/src/pages/ModDash.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../styles/ModDash.css'
+
+
+function ModDash() {
+    return (
+        <div>
+            <div className='header-section'>
+                <div>
+                    <img className='wide-header-image' src="https://images.pexels.com/photos/15382/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                
+                <div className='profile-header'>
+                    <div className='profile-pic-wrapper'>
+                        <img className='profile-pic' src="https://media.istockphoto.com/id/1393750072/vector/flat-white-icon-man-for-web-design-silhouette-flat-illustration-vector-illustration-stock.jpg?s=612x612&w=0&k=20&c=s9hO4SpyvrDIfELozPpiB_WtzQV9KhoMUP9R9gVohoU=" alt="" />
+                    </div>
+                </div>
+                </div>
+                
+            </div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+export default ModDash;
