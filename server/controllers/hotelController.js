a4fc0c6243bf1acd42e8f38f47bb4b8713a5197b
greg-supersight <grigorii.tishchuk@supersight.fi>
2024-12-01 16:49:06 +0200

    Quick fix: fixed getAllHotels to allow getting hotels without id

--- server/controllers/hotelController.js
+++ server/controllers/hotelController.js
@@ -44,7 +44,7 @@ export const getHotel = async (req, res, next) => {
 
 export const getAllHotels = async (req, res, next) => {
   try {
-    const hotel = await Hotel.findById(req.params.id);
+    const hotel = await Hotel.find();
     res.status(200).json(hotel);
   } catch (err) {
     next(err);
