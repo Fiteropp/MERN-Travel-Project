dcbff0ac3235ce5c0a2d4bb4c8f523b8246208f2
Jenni <euhenia.22@gmail.com>
2024-12-10 13:54:09 +0200

    Setup for User Dashboard

---
+++ client/react/src/pages/UserDash.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import '../styles/ModDash.css';
+import { useEffect, useState } from 'react';
+
+import ModTabs from '../components/UserTabs';
+
+
+
+function UserDash() {
+
+    const [user, setUser] = useState({});
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/api/auth/getuserdata`,{
+                method: 'GET',
+                credentials: 'include'
+            }
+                
+            );
+            const data = await response.json();
+            setUser(data);
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        } finally {
+            setLoading(false);
+        }
+        };
+
+        fetchUserData();
+    }, []);
+
+    return (
+        <div className='main-container' >
+            <div className='header-section'>
+                    <img className='wide-header-image' src="https://images.pexels.com/photos/2832061/pexels-photo-2832061.jpeg" alt="" />
+                
+                    <div className='profile-card'>
+                        
+                        <div className='profile-pic-frame'>
+                            <img  src={ user.img || "https://cdn-icons-png.flaticon.com/512/9385/9385289.png"} alt="" className='card' />
+                        </div>
+                        
+                        <div className='profile-card-info'>
+                            <h3 className='card-username'>User</h3>
+                            <p className='card-user-email'>{user?.email}</p>
+                        </div>
+                    </div>
+            </div>
+            
+            <div className='page-tabs-section'>
+                <UserTabs />
+            </div>
+            
+        </div>
+    );
+}
+
+export default UserDash;
