2e670a996de7bff4285a2f93c5707ccb0ea8ee13
Fiteropp <140279026+Fiteropp@users.noreply.github.com>
2024-12-11 14:19:12 +0200

    Image size fix

--- client/react/src/components/AboutUs.jsx
+++ client/react/src/components/AboutUs.jsx
@@ -4,7 +4,7 @@ import mern_collage from "../assets/images/mern_collage.png";
 export function AboutUs() {
   return <section className="about-us-container">
           <div className="feature-photo">
-            <img src={mern_collage} alt="" />
+            <img className="feature-img" src={mern_collage} alt="" />
           </div>
           <div className="about-section">
               <h5>About Us</h5>
--- client/react/src/components/ClientsContainer.jsx
+++ client/react/src/components/ClientsContainer.jsx
@@ -6,7 +6,7 @@ export function ClientsContainer({
         <h2 className="section_header">What our clients say</h2>
         <div className="client_grid">
           <div className="client_card">
-            <img src={client} alt="client" />
+            <img className="client-img" src={client} alt="client" />
             <p>
               The booking process was seamless, and the confirmation was
               instant. I highly recommend Mern Hotel Booking for hassle-free
@@ -14,14 +14,14 @@ export function ClientsContainer({
             </p>
           </div>
           <div className="client_card">
-            <img src={client} alt="client" />
+            <img className="client-img" src={client} alt="client" />
             <p>
               The website provided detailed information about hotel, including
               amenities, photos, which helped me make an informed decision.
             </p>
           </div>
           <div className="client_card">
-            <img src={client} alt="client" />
+            <img className="client-img" src={client} alt="client" />
             <p>
               I was able to book a room within minutes, and the hotel exceeded
               my expectations. I appreciate Mern Hotel Booking efficiency and
