9cdc1b054528e9a46ed37708f0b9a9b90543ba95
Purity-s <puritymuchiri5@gmail.com>
2024-12-12 11:22:04 +0200

    update to test autoredeployment

--- client/react/src/components/Navigation.jsx
+++ client/react/src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import "../styles/Navigation.css";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import React, { useState, useEffect } from "react";
 import { useLocation, Link, useNavigate } from "react-router-dom";
 import { getAuthToken } from "../services/authService";
@@ -12,20 +12,23 @@ const Navigation = () => {
   const [user, setUser] = useState({});
 
   useEffect(() => {
-      const fetchUserData = async () => {
-          try {
-              const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}api/auth/getuserdata`,{
-                  method: 'GET',
-                  credentials: 'include'
-              }     
-              );
-              const data = await response.json();
-              setUser(data);
-          } catch (error) {
-              console.error("Error fetching user data:", error);
+    const fetchUserData = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}api/auth/getuserdata`,
+          {
+            method: "GET",
+            credentials: "include",
           }
-      };fetchUserData();
-  },[]);
+        );
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+    fetchUserData();
+  }, []);
 
   const handleScroll = () => setScroll(window.scrollY);
 
@@ -35,14 +38,15 @@ const Navigation = () => {
 
     // Set class name dynamically based on scroll and route
     if (location.pathname === "/") {
-      setClassName(scroll > 540 ? "navbar navbar-home-scrolled" : "navbar navbar-home");
+      setClassName(
+        scroll > 540 ? "navbar navbar-home-scrolled" : "navbar navbar-home"
+      );
     } else {
       setClassName("navbar navbar-other");
     }
 
     return () => window.removeEventListener("scroll", handleScroll); // Cleanup
   }, [scroll, location.pathname]); // Re-run on scroll or route change
-
 
   const handleLogout = () => {
     document.cookie = "token=; path=/; expires=Thu, 01 Jan 1970 00:00:00 UTC"; // Clear cookie
@@ -52,7 +56,7 @@ const Navigation = () => {
 
   return (
     <nav className={className}>
-      <div className="nav_logo">Mern Hotel Booking</div>
+      <div className="nav_logo">Mern Travel Booking</div>
       <ul className="nav_links">
         <li className="link">
           <Link to="/">Home</Link>{" "}
@@ -68,22 +72,30 @@ const Navigation = () => {
         </li>
         {user && (
           <li className="link">
-            <Link to={`/userprofile/${user._id}`}>Profile</Link>{" "} 
+            <Link to={`/userprofile/${user._id}`}>Profile</Link>{" "}
           </li>
         )}
       </ul>
       <div className="buttons">
         {user ? (
           <>
-            <span className="username">{user.fullName}</span> 
-            <Button variant="outlined" className="button" onClick={handleLogout}>
+            <span className="username">{user.fullName}</span>
+            <Button
+              variant="outlined"
+              className="button"
+              onClick={handleLogout}
+            >
               Logout
             </Button>
           </>
         ) : (
           <>
-            <Button variant="text" className="button" href="/signup">SignUp</Button>
-            <Button variant="outlined" className="button" href="/login">LogIn</Button>
+            <Button variant="text" className="button" href="/signup">
+              SignUp
+            </Button>
+            <Button variant="outlined" className="button" href="/login">
+              LogIn
+            </Button>
           </>
         )}
       </div>
