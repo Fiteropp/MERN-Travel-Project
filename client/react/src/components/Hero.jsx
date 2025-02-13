d1048cb719a2d27ed21476b618f3ef85d941e482
Jenni <euhenia.22@gmail.com>
2025-02-13 16:00:01 +0200

    Added Hero.css and updated Hero.jsx. New and dynamic background images on the main page.

--- client/react/src/components/Hero.jsx
+++ client/react/src/components/Hero.jsx
@@ -1,18 +1,35 @@
-import React from "react";
-import hero from "../assets/images/hero.jpg";
+import React, { useEffect, useState } from "react";
+import hero1 from "../assets/images/hero1.jpg";
+import hero2 from "../assets/images/hero2.jpg";
+import hero3 from "../assets/images/hero3.jpg";
+import "..//styles/Hero.css"
+
 export const Hero = () => {
+  const images = [hero1, hero2, hero3];
+  const [currentImage, setCurrentImage] = useState(0);
+  const [isBlurring, setIsBlurring] = useState(false); /**/
+
+  useEffect(() =>{
+    const interval = setInterval(() =>{
+      setCurrentImage((prev)=> (prev + 1) %images.length);
+
+    }, 5000);
+    return () => clearInterval(interval);
+  },[images.length]);
+
   return (
     <div>
-      <header className="section_container header_container">
-        <div className="header_image_container">
-          <div className="header_content">
-            <h1>Enjoy Your Dream Vacation</h1>
-            <p>Book Hotels at lowest prices</p>
-          </div>
-          <div className="booking_container"></div>
-        </div>
-      </header>
+      <header 
+      className="section_container header_container"
+      style={{
+        backgroundImage: `url(${images[currentImage]})`
+      }}
+      >
+      <div className="header_content">
+      <h1>Enjoy Your Dream Vacation</h1>
+      <p>Book Hotels at lowest prices</p>
     </div>
-  );
+  </header>
+</div>
+);
 };
-
