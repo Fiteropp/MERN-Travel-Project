6491239ea70651dda1924506269face08fe3d85a
Jenni <euhenia.22@gmail.com>
2025-02-17 00:09:22 +0200

    A darkened frame with a blurr has been created so that the text does not get lost against the background of the images. The position and sizes of the frame and text have been adjusted. The size of the text frame varies depending on the size of the window.

--- client/react/src/components/Hero.jsx
+++ client/react/src/components/Hero.jsx
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from "react";
 import hero1 from "../assets/images/hero1.jpg";
 import hero2 from "../assets/images/hero2.jpg";
 import hero3 from "../assets/images/hero3.jpg";
+import hero4 from "../assets/images/hero.jpg";
 import "..//styles/Hero.css"
 
 export const Hero = () => {
-  const images = [hero1, hero2, hero3];
+  const images = [hero1, hero2, hero3, hero4];
   const [currentImage, setCurrentImage] = useState(0);
-  const [isBlurring, setIsBlurring] = useState(false); /**/
+  const [isAnimating, setIsAnimating] = useState(false); //
 
   useEffect(() =>{
     const interval = setInterval(() =>{
+      setIsAnimating(true); //
+      setTimeout(() => { //
       setCurrentImage((prev)=> (prev + 1) %images.length);
+      setIsAnimating(false); //
+      }, 4000); // Время анимации = 4 секунды
 
     }, 5000);
     return () => clearInterval(interval);
@@ -20,13 +25,14 @@ export const Hero = () => {
   return (
     <div>
       <header 
-      className="section_container header_container"
+      //className="section_container header_container"//
+      className={`section_container header_container ${isAnimating ? "animated" : ""}`}
       style={{
         backgroundImage: `url(${images[currentImage]})`
       }}
       >
-      <div className="header_content">
-      <h1>Enjoy Your Dream Vacation</h1>
+      <div className="header_contentHero">
+      <h2>Enjoy Your Dream Vacation</h2>
       <p>Book Hotels at lowest prices</p>
     </div>
   </header>
